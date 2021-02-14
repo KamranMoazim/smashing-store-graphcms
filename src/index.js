3f7d1650bc6d116aaa068441e23e5a7ea459b298
CamrunMoazim <kamrannaseer765@gmail.com>
2021-02-14 19:53:20 +0500

    added

--- src/index.js
+++ src/index.js
@@ -2,16 +2,20 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+import { ApolloProvider } from "react-apollo";
+import ApolloClient from "apollo-boost";
+import 'bootstrap/dist/css/bootstrap.css';
+// import * as serviceWorker from './serviceWorker';
+
+const client = new ApolloClient({
+  uri: "https://api-ap-northeast-1.graphcms.com/v2/ckl56x5t8m8p701xsamkaarp7/master"
+});
 
 ReactDOM.render(
-  <React.StrictMode>
+  <ApolloProvider client={client}>
     <App />
-  </React.StrictMode>,
+  </ApolloProvider>,
   document.getElementById('root')
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+// serviceWorker.unregister();
